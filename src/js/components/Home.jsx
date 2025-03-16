4ca3ff925f21a7f55fcee215ee98b0a4433e3b1d
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-16 19:41:15 +0000

    añadidas las cartas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
-
+import Card from "./card.jsx";
 //include images into your bundle
 
+const cards = [
+	{
+		image: "https://picsum.photos/id/10/200/300",
+		title: "Parque",
+		description: "Parque con lindo paisaje",
+	},
+	{
+		image: "https://picsum.photos/id/17/200/300",
+		title: "Sendero",
+		description: "Sendero con lindo paisaje",
+	},
+	{
+		image: "https://picsum.photos/id/28/200/300",
+		title: "Bosque",
+		description: "Bosque con lindo paisaje",
+	},
+	{
+		image: "https://picsum.photos/id/54/200/300",
+		title: "Montaña",
+		description: "Montaña con lindo paisaje",
+	}
+];
 
 //create your first component
 const Home = () => {
@@ -12,7 +34,12 @@ const Home = () => {
 			<Navbar/>   
 			<Jumbotron/>
 			<div className="container">
-				<div className="row">					
+				<div className="row">
+					{cards.map((item,index) => (
+						<div className="col-12 col-sm6 col-md-4 col-lg-3 mb-4" key={index}>
+							<Card cards={item} />							
+						</div>
+					))}					
 				</div>
 			</div>         
 
